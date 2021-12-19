c653578c29be191a859c0a5625521d133c3fc362
Chris Duffy <christopher.b.duffy@gmail.com>
2021-12-19 10:10:48 -0800

    Added keypress handler

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const createScene = function () {
     
     const scene = new BABYLON.Scene(engine);  
 
-    BABYLON.MeshBuilder.CreateBox("box", {})
-
+    
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10});
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
@@ -15,6 +15,15 @@ const createScene = function () {
 };
 
 const scene = createScene();
+var player = BABYLON.MeshBuilder.CreateSphere("sphere", {})
+player.position.y = 0.5;
+
+for (var i=0;i<15;i++){
+    var box = BABYLON.MeshBuilder.CreateBox("box", {})
+    box.position.y = 0.5;
+    box.position.x = Math.floor(Math.random()*10)-5;
+    box.position.z = Math.floor(Math.random()*10)-5; 
+}
 
 engine.runRenderLoop(function (){
     scene.render();
@@ -24,3 +33,19 @@ window.addEventListener("resize",function(){
     engine.resize();
 });
 
+function handleKeyDown(event){
+    if (event.key=='w'){
+        player.position.z--;
+    }
+    if (event.key=='s'){
+        player.position.z++;
+    }
+    if (event.key=='a'){
+        player.position.x--;
+    }
+    if (event.key=='d'){
+        player.position.x++;
+    }
+}
+
+document.addEventListener('keydown',handleKeyDown);
