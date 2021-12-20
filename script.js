a2647041a689ff711d22827541818c8b00b4c1c1
Chris Duffy <christopher.b.duffy@gmail.com>
2021-12-20 13:11:21 -0800

    Added Follow Camera

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 const canvas = document.getElementById("babylonCanvas");
 const engine = new BABYLON.Engine(canvas,true);
-
+var player;
 const createScene = function () {
     
     const scene = new BABYLON.Scene(engine);  
 
     
     const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10});
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
-    camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+
+    // This creates and initially positions a follow camera 	
+    var camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 10, -10), scene);
+	
+	//The goal distance of camera from target
+	camera.radius = 30;
+	
+	// The goal height of camera above local origin (centre) of target
+	camera.heightOffset = 20;
+	
+	// The goal rotation of camera around local origin (centre) of target in x y plane
+	camera.rotationOffset = 0;
+	
+	//Acceleration of camera in moving from current to goal position
+	camera.cameraAcceleration = 0.05
+	
+	//The speed at which acceleration is halted 
+	camera.maxCameraSpeed = 10
+
+    BABYLON.SceneLoader.ImportMesh(null, "meshes/", "car.babylon", scene, function (newMeshes) {
+        // Set the target of the camera to the first imported mesh
+        player = newMeshes[0];
+        player.scaling.x=.5;
+        player.scaling.y=.5;
+        player.scaling.z=.5;
+        player.position.y=1;
+        camera.lockedTarget = player;
+    });
 
     return scene;
 };
@@ -22,19 +48,15 @@ for (var i=0;i<15;i++){
     box.position.x = Math.floor(Math.random()*10)-5;
     box.position.z = Math.floor(Math.random()*10)-5; 
 }
-var player;
-BABYLON.SceneLoader.ImportMesh(null, "meshes/", "car.babylon", scene, function (newMeshes) {
-    // Set the target of the camera to the first imported mesh
-    player = newMeshes[0];
-    player.scaling.x=.5;
-    player.scaling.y=.5;
-    player.scaling.z=.5;
-    player.position.y=1;
-});
 
-const camera = new BABYLON.FollowCamera("camera", new BABYLON.Vector3(5, 5, 0), scene, player);
-camera.lockedTarget = player;
+
+
+
+
+
+
 engine.runRenderLoop(function (){
+    
     scene.render();
 });
 
