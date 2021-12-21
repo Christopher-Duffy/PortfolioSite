94d2f46bc308778d0a3fbdd25bd76113bd81332b
Chris Duffy <christopher.b.duffy@gmail.com>
2021-12-21 12:37:58 -0800

    Added Textures

--- script.js
+++ script.js
@@ -1,55 +1,95 @@
+
 const canvas = document.getElementById("babylonCanvas");
 const engine = new BABYLON.Engine(canvas,true);
 var player;
 var camera;
+var shadowGenerator;
 const createScene = function () {
     
-    const scene = new BABYLON.Scene(engine);  
-    
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:1000, height:1000});
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+    const scene = new BABYLON.Scene(engine);
 
-    // This creates and initially positions a follow camera 	
-    camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 10, -10), scene);
-	
-	//The goal distance of camera from target
-	camera.radius = 30;
-	
-	// The goal height of camera above local origin (centre) of target
-	camera.heightOffset = 20;
-	
-	// The goal rotation of camera around local origin (centre) of target in x y plane
-	camera.rotationOffset = 0;
-	
-	//Acceleration of camera in moving from current to goal position
-	camera.cameraAcceleration = 0.05
-	
-	//The speed at which acceleration is halted 
-	camera.maxCameraSpeed = 10
+    var gravityVector = new BABYLON.Vector3(0,-9.81, 0);
+    var physicsPlugin = new BABYLON.CannonJSPlugin();
+    scene.enablePhysics(gravityVector, physicsPlugin);
 
-    BABYLON.SceneLoader.ImportMesh(null, "meshes/", "car.babylon", scene, function (newMeshes) {
-        // Set the target of the camera to the first imported mesh
-        player = newMeshes[0];
-        player.scaling.x=.5;
-        player.scaling.y=.5;
-        player.scaling.z=.5;
-        player.position.y=5;
-        camera.lockedTarget = player;
-        scene.doBeforeRender=function(){
-            camera.rotationOffset=player.rotation.y/Math.PI*180;
-        }
+    //const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:1000, height:1000});
+
+    var grassMat = new BABYLON.StandardMaterial("grass");
+
+	grassMat.diffuseTexture = new BABYLON.Texture("grassTile.png");
+    grassMat.bumpTexture = new BABYLON.Texture("grassNormalMap.png");
+    grassMat.diffuseTexture.uScale = 50.0;
+    grassMat.diffuseTexture.vScale = 50.0;
+    grassMat.bumpTexture.uScale = 50.0;
+    grassMat.bumpTexture.vScale = 50.0;
+    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "HeightMap.png", 1000, 1000, 500, 0, 10, scene, false, function () {
+		ground.imposter = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.HeightmapImpostor, { mass: 0 });
+        ground.material = grassMat;    
     });
     
+    //ground.impostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    var light = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(-1, -2, -1), scene);
+	light.position = new BABYLON.Vector3(20, 40, 20);
+	light.intensity = 0.5;
+    
+    shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
+    ground.receiveShadows = true;
+    
+    scene.ambientColor = new BABYLON.Color3(1, 1, 1);
+
+
+
+    // This creates and initially positions a follow camera 	
+    var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(5, 50, -50), scene);
+
+// Targets the camera to a particular position. In this case the scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    
+    player = BABYLON.MeshBuilder.CreateSphere("player", {})
+    player.position.y=20;
+    player.imposter = new BABYLON.PhysicsImpostor(player, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 10, restitution: 0.9 }, scene);
+    shadowGenerator.addShadowCaster(player);
+    var myMaterial = new BABYLON.StandardMaterial("playerMaterial", scene);
+
+    myMaterial.diffuseColor = new BABYLON.Color3(0, 1, 0);
+    myMaterial.specularColor = new BABYLON.Color3(0, 0.2, 0.2);
+    myMaterial.ambientColor = new BABYLON.Color3(0,0,1);
+
+    player.material = myMaterial;
+
+    camera.lockedTarget = player;
+    
+     scene.registerBeforeRender(function(){
+         camera.position.x=player.position.x+5;
+         camera.position.y=player.position.y+50;
+         camera.position.z=player.position.z-50;
+
+    });
+
     return scene;
 };
-
 const scene = createScene();
 
-for (var i=0;i<150;i++){
-    var box = BABYLON.MeshBuilder.CreateBox("box", {})
+
+
+for (var i=0;i<10;i++){
+    var box = BABYLON.MeshBuilder.CreateBox("box", {});
     box.position.y = Math.floor(Math.random()*5);
     box.position.x = Math.floor(Math.random()*100)-50;
-    box.position.z = Math.floor(Math.random()*100)-50; 
+    box.position.z = Math.floor(Math.random()*100)-50;
+    new BABYLON.PhysicsImpostor(box, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, restitution: 0.9 }, scene);
+
+    //shadowGenerator.addShadowCaster(box);
+
+    var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+
+    myMaterial.diffuseColor = new BABYLON.Color3(1, 0, 1);
+    myMaterial.specularColor = new BABYLON.Color3(0.5, 0.6, 0.87);
+    myMaterial.ambientColor = new BABYLON.Color3(1,0,0);
+
+    box.material = myMaterial;
+
 }
 
 
@@ -59,7 +99,6 @@ for (var i=0;i<150;i++){
 
 
 engine.runRenderLoop(function (){
-    scene.doBeforeRender();
     scene.render();
 });
 
@@ -69,17 +108,16 @@ window.addEventListener("resize",function(){
 
 function handleKeyDown(event){
     if (event.key=='w'){
-        camera.rotationOffset=player.rotation.y/Math.PI*180;
-        player.rotation.y+=1;
+        player.imposter.applyImpulse(new BABYLON.Vector3(0,0,5),player.getAbsolutePosition());
     }
     if (event.key=='s'){
-        player.position.z++;
+        player.imposter.applyImpulse(new BABYLON.Vector3(0,0,-5),player.getAbsolutePosition());
     }
     if (event.key=='a'){
-        player.position.x++;
+        player.imposter.applyImpulse(new BABYLON.Vector3(-5,0,0),player.getAbsolutePosition());
     }
     if (event.key=='d'){
-        player.position.x--;
+        player.imposter.applyImpulse(new BABYLON.Vector3(5,0,0),player.getAbsolutePosition());
     }
 }
 
